183bf81e61d14aa8e9e847b505e8474d33721735
DakotaLai <97612909+DakotaLai@users.noreply.github.com>
2024-04-19 20:02:57 -0400

    ababbaba

--- Kanbas/quizQuestions/schema.js
+++ Kanbas/quizQuestions/schema.js
@@ -1,24 +1,17 @@
 import mongoose from "mongoose";
 const choiceSchema = new mongoose.Schema({
-    question: {type: String, required: true},
-    choices: [{
-        _id: {type: String, },
-        text: {type: String, },
-        isCorrect:{type: Boolean, default: false}
-    }]
+    _id: {type: String},
+    text: {type: String},
+    isCorrect:{type: Boolean, default: false}  
 });
 const trueFalseSchema = new mongoose.Schema({
-    question: {type: String,},
     _id: {type: String, },
     isTrue: {type: Boolean, }
 });
 const blankSchema = new mongoose.Schema({
-    question: {type: String, },
-    blanks: [{
-        _id: {type: String, },
-        correctAnswers: [{type: String, }],
-        caseInsensitive: {type: Boolean, default: false}
-    }]
+    _id: {type: String, },
+    correctAnswers: {type: String, },
+    caseInsensitive: {type: Boolean, default: false}
 });
 const quizQuestionSchema = new mongoose.Schema({
     _id: String,
@@ -26,18 +19,14 @@ const quizQuestionSchema = new mongoose.Schema({
     title: { type: String, required: true},
     question: {type: String, required: true},
     points: Number,
-    questionType: {
+    questionType: { 
         type: String,
         required: true,
         enum: ["Multiple Choice", "True/False", "Fill In the Blank"],
         default: "Multiple Choice",},
-    multipleChoice: {
-        choices: [choiceSchema]  
-    },
+    multipleChoice:[choiceSchema],
     trueFalse: [trueFalseSchema],
-    fillBlank: {
-        fillBlank: [blankSchema]
-    },
+    fillBlank: [blankSchema],
   },
   { collection: "quiz questions" });
 export default quizQuestionSchema;
--- Kanbas/quizzes/dao.js
+++ Kanbas/quizzes/dao.js
@@ -1,11 +1,15 @@
 import model from "./model.js";
 import { v4 as uuidv4 } from 'uuid';
 
-export const createQuiz = (courseId, quiz) => {
-    delete quiz._id;
-    quiz._id = uuidv4();
+export const createQuiz = async (courseId, quiz) => {
+    // delete quiz._id;
+    // quiz._id = uuidv4();
+    // quiz.course = courseId;
+    // courseId = model.find({course: courseId});
+    const maxId = await model.findOne({}, {_id: 1}, {sort: {_id: -1}});
+    const numericalPart = maxId ? parseInt(maxId._id.slice(1)) + 1 : 101;
+    quiz._id = "Q" + numericalPart.toString();
     quiz.course = courseId;
-    courseId = model.find({course: courseId});
     return model.create(quiz);
   } 
 export const findQuizzesForCourse = (courseId) => model.find({course: courseId});
